980aaeadd16c89aa25a9bba6729a157102940fb5
ga1i13o <gabriele.trivigno@studenti.polito.it>
2019-05-27 19:23:31 +0200

    added delete functionality

--- static/todolist.js
+++ static/todolist.js
@@ -1,4 +1,4 @@
-const RESTAPI = 'http://localhost:5000/api/v1.0';
+const RESTAPI = 'http://localhost:5000/api/v1';
 const NiceButtons = '<td><a href="#"><img class="actionicon actiondelete" src="https://img.icons8.com/windows/32/000000/delete/2266EE"></a>' +
     '<a href="#"><img class="actionicon actionedit" src="https://img.icons8.com/windows/32/000000/edit/2266EE"></a></td>';
 
@@ -18,5 +18,71 @@ $(document).ready(function () {
     });
 
 
+    //insert new task
+    $('#newtask').click(function (event) {
+        event.preventDefault();
+
+        var descr = $("input[name='tasktext']").val();
+        var urg = $("button#taskurgent").hasClass('active').toString();
+
+        var data = JSON.stringify({
+            'description': descr,
+            'urgent': urg
+        });
+
+        //call rest api
+        $.ajax({
+            'url': RESTAPI + "/tasks",
+            'method': "POST",
+            'data': data,
+            'contentType': "application/json",
+            'success':function () {
+                refreshTasks();
+            }
+        });
+    });
+
+    function refreshTasks(){
+
+        //get list and rebuild
+        $.ajax({
+            'url':RESTAPI+"/tasks",
+            'method':"GET",
+            'dataType':"json",
+            'success': function (data, textStatus, jqXHR) {
+                $('tbody#tasklistbody').empty();
+
+                for( i=0; i < data['tasks'].length; i++){
+                    var description = data['tasks'][i]['todo'];
+                    var id = data['tasks'][i]['id'];
+                    var urgent = data['tasks'][i]['urgency'];
+
+                    $('tbody#tasklistbody').append(
+                    "<tr data-id='"+id+"'> <td>"+description+"</td>"+
+                    "<td>"+urgent+"</td>"+ NiceButtons +"</tr>"
+                    );
+                }
+
+                $(".actiondelete").click(function (event) {
+                         event.preventDefault();
+
+                         var id = $(this).closest("tr").data("id");
+                         $.ajax({
+                            'url': RESTAPI+"/tasks/"+id,
+                            'method':"DELETE",
+                            'success': function () {
+                                refreshTasks();
+                            }
+                    });
+    });
+            }
+        });
+    }
+
+    //startup task
+    refreshTasks();
+
+
 });
+
 
